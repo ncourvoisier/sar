8b6a99e18a631df2e1264da67dce1d3089817ca3
Cédric <45012770+cedricPoncot@users.noreply.github.com>
2018-11-23 13:17:35 +0100

    testJS

--- JS/sar.js
+++ JS/sar.js
@@ -1,1 +1,7 @@
-
+function recupValeur(){
+val=document.forms["Requete"].elements["Table1"].value;
+console.log("Valeur premiere table"+val);
+}
+function recupValeur1(){
+console.log("Valeur premiere table");
+}

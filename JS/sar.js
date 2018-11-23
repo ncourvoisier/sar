344b911b2a7195b5338506746e221b4c2078fa74
Cédric <45012770+cedricPoncot@users.noreply.github.com>
2018-11-23 13:21:14 +0100

    Recuperation des valeurs JS

--- JS/sar.js
+++ JS/sar.js
@@ -1,4 +1,10 @@
 function recupValeur(){
-val=document.forms["Requete"].elements["Table1"].value;
-console.log("Valeur premiere table"+val);
+val1=document.forms["Requete"].elements["Table1"].value;
+console.log("Valeur premiere table ="+val1);
+
+op=document.forms["Requete"].elements["operateur"].value;
+console.log("Valeur operateur ="+op);
+
+val2=document.forms["Requete"].elements["Table2"].value;
+console.log("Valeur deuxieme table ="+val2);
 }
